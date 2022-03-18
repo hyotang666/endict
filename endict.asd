c7f8c3df21710f355998445a7232b364482c831b
SATO Shinichi <hyotang.karakoma@gmail.com>
2022-03-18 20:36:41 +0900

    Fix: SECTION to ignore meaningless line.

--- endict.asd
+++ endict.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "endict"
   :version
-  "0.3.1"
+  "0.3.2"
   :depends-on
   (
    "uiop"       ; Utilities, implicitly depends on via asdf.
--- src/endict.lisp
+++ src/endict.lisp
@@ -23,28 +23,30 @@
 1. A list of the lines.
 2. A name of next section if exists otherwise NIL."
   (do ((line #0=(slurp-line input) #0#)
-       acc)
+       acc
+       (ignore '("M." "P." "X.")))
       ((null line) (values (cons name (nreverse acc)) nil))
     (if acc
-        (if (and ;; The name must have one alphabet at least.
-                 (find-if #'upper-case-p line)
-                 (every
-                   (lambda (c) (or (upper-case-p c) (not (alphanumericp c))))
-                   line)
-                 ;; The name never have bracket nor paren.
-                 (not (find-if (lambda (char) (find char "[]()")) line))
-                 ;; The name never start with "&".
-                 (not (uiop:string-prefix-p "&" line))
-                 ;; The name never have "..".
-                 (not (search ".." line))
-                 ;; Next name must start with same alphabet or next alphabet of the NAME.
-                 (or (string= line name :end1 1 :end2 1)
-                     (char= (char line 0)
-                            (code-char (1+ (char-code (char name 0))))))
-                 ;; The name line must below empty line.
-                 (equal "" (car acc)))
-            (return (values (cons name (nreverse acc)) line))
-            (push line acc))
+        (unless (member line ignore :test #'equal)
+          (if (and ;; The name must have one alphabet at least.
+                   (find-if #'upper-case-p line)
+                   (every
+                     (lambda (c) (or (upper-case-p c) (not (alphanumericp c))))
+                     line)
+                   ;; The name never have bracket nor paren.
+                   (not (find-if (lambda (char) (find char "[]()")) line))
+                   ;; The name never start with "&".
+                   (not (uiop:string-prefix-p "&" line))
+                   ;; The name never have "..".
+                   (not (search ".." line))
+                   ;; Next name must start with same alphabet or next alphabet of the NAME.
+                   (or (string= line name :end1 1 :end2 1)
+                       (char= (char line 0)
+                              (code-char (1+ (char-code (char name 0))))))
+                   ;; The name line must below empty line.
+                   (equal "" (car acc)))
+              (return (values (cons name (nreverse acc)) line))
+              (push line acc)))
         (if (and (not (equal "" line))
                  (every (lambda (c) (or (upper-case-p c) (char= #\Space c)))
                         line))
